3c605009ea0e25dd4150185a8d170f7860d2dee7
Srikaanth-19 <srikaanth198@gmail.com>
2025-05-27 19:27:19 +0530

    Create _26.Threads.java

---
+++ Module 3-JAVA/_26.Threads.java
@@ -0,0 +1,22 @@
+class MyThread extends Thread {
+    private String message;
+
+    MyThread(String message) {
+        this.message = message;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(message + " " + i);
+        }
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread 1");
+        MyThread t2 = new MyThread("Thread 2");
+        t1.start();
+        t2.start();
+    }
+}
