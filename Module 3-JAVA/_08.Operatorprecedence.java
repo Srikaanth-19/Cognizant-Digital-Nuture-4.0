64fe1ebedd3246edc6d0e2d1347e702ef3b44f57
Srikaanth-19 <srikaanth198@gmail.com>
2025-05-27 18:54:58 +0530

    Create _08.Operatorprecedence.java

---
+++ Module 3-JAVA/_08.Operatorprecedence.java
@@ -0,0 +1,8 @@
+public class Operatorprecedence {
+    public static void main(String[] args) {
+        int result = 15 + 6 * 9;
+        System.out.println("Result of 15 + 3 * 9 = " + result); 
+        int result2 = (10 + 5) * 2;
+        System.out.println("Result of (10 + 5) * 2 = " + result2); 
+    }
+}
