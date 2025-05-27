f3b26ec59f1d341105fcd1e31b04696b80037c16
Srikaanth-19 <srikaanth198@gmail.com>
2025-05-27 18:18:40 +0530

    Create Helloworld.java

---
+++ Module 3 - java/Helloworld.java
@@ -0,0 +1,8 @@
+// 1. Hello World Program
+// Objective: Understand the basic structure of a Java program.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
