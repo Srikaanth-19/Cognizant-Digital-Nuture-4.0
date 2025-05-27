bbea9bdbf9065573071a82ddda1e0e7d542ec4c7
Srikaanth-19 <srikaanth198@gmail.com>
2025-05-27 19:40:37 +0530

    Create Tcp_client.java

---
+++ Module 3-JAVA/Tcp_client.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+public class TCP_client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 1234);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        String userInput;
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            System.out.println("Server: " + in.readLine());
+        }
+    }
+}
