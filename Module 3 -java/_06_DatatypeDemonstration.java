f88d1d1988754124851e2496ce1b632e80f098b2
Srikaanth-19 <srikaanth198@gmail.com>
2025-05-27 18:32:30 +0530

    Create _06_DatatypeDemonstration.java

---
+++ Module 3 -java/_06_DatatypeDemonstration.java
@@ -0,0 +1,18 @@
+// 6. Data Type Demonstration
+// Objective: Understand Java's primitive data types.
+
+public class DataTypeDemonstration {
+    public static void main(String[] args) {
+        int num = 98;
+        float rate = 16.5f;
+        double value = 187.006;
+        char grade = 'C';
+        boolean status = true;
+
+        System.out.println("Integer: " + num);
+        System.out.println("Float: " + rate);
+        System.out.println("Double: " + value);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + status);
+    }
+}
