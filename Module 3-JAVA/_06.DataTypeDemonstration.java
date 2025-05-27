cb56c78aefb77dd34da0f8350f6e5057a1764449
Srikaanth-19 <srikaanth198@gmail.com>
2025-05-27 18:52:48 +0530

    Create _06.DataTypeDemonstration.java

---
+++ Module 3-JAVA/_06.DataTypeDemonstration.java
@@ -0,0 +1,15 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int num = 27;
+        float rate = 10.5f;
+        double value = 123.456;
+        char grade = 'A';
+        boolean status = true;
+
+        System.out.println("Integer: " + num);
+        System.out.println("Float: " + rate);
+        System.out.println("Double: " + value);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + status);
+    }
+}
